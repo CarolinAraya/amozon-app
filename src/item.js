fc028e50b8cc669a41a87bbbb3bb3d27b6bd1e6e
Carolina <caro.frontend@gmail.com>
2021-05-06 11:44:20 -0400

    mapeo de rating

--- src/item.js
+++ src/item.js
@@ -2,19 +2,37 @@ import React from 'react';
 import './item.css';
 
 
-class Item extends React.Component {
+class Item extends React.Component {//para poder usar el estado y usar la información
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stars: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({//Modifica el estado
+      stars: Array(parseInt(this.props.rating)).fill(0)
+    });
+  }
 
   render() {
     return (
       <div className="item" >
-        <div className="image"><img src={'img/' + props.image} width="100%" /></div>
-        <div className="title">{props.title}</div>
+        <div className="image"><img src={'img/' + this.props.image} width="100%" /></div>
+        <div className="title">{this.props.title}</div>
         <div className="rating">
           <p>
-
+            {
+              this.state.stars.map(x =>
+                <img src="img/start.png" width="32" />
+              )
+            }
           </p>
           Calificación:
-          <select value={props.rating}>
+          <select value={this.props.rating}>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
